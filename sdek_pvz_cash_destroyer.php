6940fe31896c974d6e565be9d53bbc445137bb7a
DmitrySolo <solo-webworks@mail.ru>
2018-05-16 18:45:02 +0300

    initial commit

---
+++ sdek_pvz_cash_destroyer.php
@@ -0,0 +1,5 @@
+<?
+if(!file_exists('/var/www/west/data/www/santehsmart.ru/punkty-vydachi/cdekRequest.txt)')){
+  echo  unlink('/var/www/west/data/www/santehsmart.ru/punkty-vydachi/cdekRequest.txt');
+}
+?>
